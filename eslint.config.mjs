19d5f66c369eb15e1ac1205aa16eadf6a7851488
piyo <chick.p.1120@gmail.com>
2024-06-29 13:39:04 +0900

    chore: use globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,17 @@
 import js from "@eslint/js";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    "rules": {
+      "no-console": "warn"
+    }
+  }
 ];
